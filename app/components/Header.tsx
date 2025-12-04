6eed4fa8f56da0470ea22183de188ac0e0ff06b7
JerMontoya <jeralyn.haycock@gmail.com>
2025-12-04 12:00:14 -0900

    Link back to home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="w-full flex items-center justify-between py-5 px-8">
       {/* Left side */}
       <div className="flex items-center gap-4">
-        <button className="text-[10px] font-semibold cursor-pointer">SKINSTRIC</button>
+        <Link href="/" className="text-[10px] font-semibold cursor-pointer">SKINSTRIC</Link>
         <span className="text-gray-400 text-[10px] font-semibold">[ INTRO ]</span>
       </div>
 
